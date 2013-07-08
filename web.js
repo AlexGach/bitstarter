0a40c08acfcc5691a1510043be14ff163effea66
alex garcia <alex.garcia.chicano@gmail.com>
2013-07-08 16:26:37 +0000

    Added web.js with index.htm

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 buffer=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
