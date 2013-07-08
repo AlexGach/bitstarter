7c879df0892ff6b54e40a4944f8b30e3022c26c4
alex garcia <alex.garcia.chicano@gmail.com>
2013-07-08 16:32:14 +0000

    Added web.js with index.htm

--- web.js
+++ web.js
@@ -2,10 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer();
 var fs = require('fs');
-
-buffer=fs.readFileSync('index.html');
+var buffer =new Buffer(fs.readFileSync('index.html'),'utf-8');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
